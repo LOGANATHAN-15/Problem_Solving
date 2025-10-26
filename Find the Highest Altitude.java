1b381b4c86c4d6e58824c3003da9bbba1db87002
LOGANATHAN-15 <loganathanmayilsamy15@gmail.com>
2025-10-26 22:59:46 +0530

    Day 11

---
+++ Find the Highest Altitude.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int len=gain.length;
+        int max=0;
+        int val=0;
+        for(int i=0;i<len;i++){
+            val+=gain[i];
+            max=Math.max(max,val);
+
+        }
+        return max;
+
+    }
+}
