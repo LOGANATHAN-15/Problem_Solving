5adfebef46d04d64716dacdf238a165eadc40edf
LOGANATHAN-15 <loganathanmayilsamy15@gmail.com>
2025-10-27 15:24:26 +0530

    Day 12

---
+++ clear digits.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String clearDigits(String s) {
+        Stack <Character> st = new Stack<>();
+        for(int i = 0;i<s.length();i++){
+            if(!st.isEmpty() && Character.isDigit(s.charAt(i))){
+                st.pop();
+            }
+            else{
+                st.push(s.charAt(i));
+            }
+        }
+    StringBuilder str = new StringBuilder();
+       while(! st.isEmpty()){
+        str.append(st.pop());
+       } 
+       str.reverse();
+       return str.toString();
+    }
+}
